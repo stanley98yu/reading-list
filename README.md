7ccac07c61c678c7c69d8a218d7aa352386ab991
Stanley Yu <stanley98yu@gmail.com>
2018-02-10 00:23:30 -0500

    Added styling for search and results functions.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Reading List Web Application
-Simple web application for creating reading lists to save favorite books for multiple users. Uses Flask for building the application and Foundation CSS for the front-end framework. Following the DevFest Web Development Track as of January 21, 2018: https://learn.devfe.st/.
+Simple web application for creating reading lists to save favorite books for multiple users. Uses Flask for building the application, Foundation CSS for the front-end framework, and the Google Books API for the search function. Following the DevFest Web Development Track as of January 21, 2018: https://learn.devfe.st/.
 
 Languages: Python, HTML/CSS/Javascript
 
--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+# app.py: Basic Flask program for running the reading list application.
+# Author: Stanley Yu
+# Date: 2/9/18
+from flask import Flask, render_template, request
 import requests
 
 
@@ -9,9 +12,18 @@ app.config["DEBUG"] = True # Only include when testing app.
 def hello():
     return render_template("index.html")
 
-@app.route("/search")
+@app.route("/search", methods=["POST", "GET"])
 def search():
-    return render_template("search.html")
+	if request.method == "POST": # User used the search box.
+		url = "https://www.googleapis.com/books/v1/volumes?q=" + request.form["user_search"]
+		response_dict = requests.get(url).json()
+		return render_template("results.html", api_data = response_dict)
+	else:
+		return render_template("search.html")
+
+@app.route("/results")
+def results():
+	return render_template("results.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
